23d6ee64b8e16e698c8bedcaf9cc77aebe0c154d
kmahipus <k.mahipus@irri.org>
2014-02-06 06:06:29 +0000

    Model.java edited online with Bitbucket

--- src/com/pedigreeimport/restjersey/Model.java
+++ src/com/pedigreeimport/restjersey/Model.java
@@ -542,163 +542,208 @@ public class Model {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response searchGID(JSONObject data) throws JSONException, FileNotFoundException,
-	IOException, MiddlewareQueryException, ParseException, ConfigException, URISyntaxException {
-
-		//new Editor().generatePedigreeTreeJson((JSONObject)data);
-		//return Response.status(200).entity("OK!").build();
-		File myFile = new File("E:/xampp/htdocs/GMGR/json_files/tree.json");
-		myFile.delete();
-
+			IOException, MiddlewareQueryException, ParseException, ConfigException, URISyntaxException {
+		
+		ManagerFactory factory = new Config().configDB();
+		GermplasmDataManager man = factory.getGermplasmDataManager();
+		List<Name> names = new ArrayList<Name>();//null;//man.getNamesByGID(node.getGermplasm().getGid(), null, null);
+		Location loc2 = new Location();
+		List<Attribute> attributes = new ArrayList<Attribute>();//man.getAttributesByGID(node.getGermplasm().getGid());
+		Method meth = new Method();//man.getMethodByID(node.getGermplasm().getMethodId());
+		Location loc = new Location();//man.getLocationByID(node.getGermplasm().getLocationId());
+		Bibref bibref = new Bibref();//man.getBibliographicReferenceByID(node.getGermplasm().getReferenceId());
+		
+		Integer cid = 0;//loc.getCntryid();
+		Country cnty = new Country();//man.getCountryById(cid);
+		
 		JSONObject json_array = (JSONObject) data;
 		String gid = (String) json_array.get("GID");
 		String level = (String) json_array.get("LEVEL");
 		JSONObject outputTree = new JSONObject();
-
-		ManagerFactory factory = new Config().configDB();
-
+		
+		//ManagerFactory factory = new Config().configDB();
+		
 		cnt = counter++;
-
+		
 		if(cnt % 2 == 1)
 		{
-
+			
 			PedigreeDataManager pedigreeManager = factory.getPedigreeDataManager();
-
-			Debug.println(10, "GID = " + Integer.parseInt(gid) + ", level = " + Integer.parseInt(level) +  ":");
-			GermplasmPedigreeTree tree = pedigreeManager.generatePedigreeTree(Integer.parseInt(gid),Integer.parseInt(level));
-
-			for(int i=0;i<Integer.parseInt(level);i++)
-			{
-				counterArray.add(0);
-			}
-
-			if (tree != null) 
-			{
-				outputString = outputString +"{";
-				System.out.println("{");
-				printNode(tree.getRoot(), 1);
-				outputString = outputString +"\n}";
-				System.out.println("}");
-			}
-
-			factory.close();
+			    
+	        Debug.println(10, "GID = " + Integer.parseInt(gid) + ", level = " + Integer.parseInt(level) +  ":");
+	        GermplasmPedigreeTree tree = pedigreeManager.generatePedigreeTree(Integer.parseInt(gid),Integer.parseInt(level));
+	   
+	        for(int i=0;i<Integer.parseInt(level);i++)
+	        {
+	        	counterArray.add(0);
+	        }
+	        
+	        if (tree != null) 
+	        {
+	        	outputString = outputString +"{";
+	        	System.out.println("{");
+	            printNode(tree.getRoot(), 1,names,loc2,attributes,cid,cnty,man,meth,bibref,loc);
+	            outputString = outputString +"\n}";
+	            System.out.println("}");
+	        }
+	        
+		   tree = null;
+	       pedigreeManager = null;
+	       
 		}
-
+		
 		outputTree.put("tree", outputString);
-
+		
 		System.out.println("Output: " + outputTree.get("tree"));
+		
+		counterArray.clear();
+		names = null;
+		loc2 = null;
+		attributes = null;
+		cid = null;
+		cnty = null;
+		meth = null;
+		bibref = null;
+		loc= null;
+		
+		factory.close();
 		return Response.status(200).entity(outputTree).build();
 	}
-
-	private void printNode(GermplasmPedigreeTreeNode node, int level) throws IOException, MiddlewareQueryException {
-
-		StringBuffer tabs = new StringBuffer();
-		ManagerFactory factory = new Config().configDB();
+	
+	private void editor() throws IOException, MiddlewareQueryException {
+		
+		ManagerFactory factory = new Config().configDB2();
 		GermplasmDataManager man = factory.getGermplasmDataManager();
-
-		Method meth = man.getMethodByID(node.getGermplasm().getMethodId());
-		Location loc = man.getLocationByID(node.getGermplasm().getLocationId());
-		Bibref bibref = man.getBibliographicReferenceByID(node.getGermplasm().getReferenceId());
-		List<Name> names = man.getNamesByGID(node.getGermplasm().getGid(), null, null);
-		Location loc2 = null;
-
-		Integer cid = loc.getCntryid();
-		Country cnty = man.getCountryById(cid);
-
-		for (int ctr = 1; ctr < level; ctr++) {
-			tabs.append("\t");
-		}
-
-		counterArray.set(level-1, 0);
-		String name = node.getGermplasm().getPreferredName() != null ? node.getGermplasm().getPreferredName().getNval() : null;
-
-		if(!node.getLinkedNodes().isEmpty()){
-			Debug.println(0, tabs.toString() +"\"gid\" : \""+ node.getGermplasm().getGid() + "\",\n" + tabs.toString()+ "\"name\" : \""+ name +"\",\n" +tabs.toString()+ "\"layer\": \""+ (level-1) + "\",");
-
-			outputString = outputString + "\n "+ tabs.toString() + tabs.toString() +"\"gid\" : \" " + node.getGermplasm().getGid() +"\",\n"
-			+ tabs.toString() +"    \"name\" : \" " + name +"\",\n"
-			+ tabs.toString() +"    \"date\" : \" " + node.getGermplasm().getGdate() +"\",\n"
-			+ tabs.toString() +"    \"id\" : \" " + node.getGermplasm().getGid() +"\",\n"
-			+ tabs.toString() +"    \"layer\" : \" " + (level-1) +"\",\n"
-			+ tabs.toString() +"    \"methodname\" : \" " + meth.getMname() +"\",\n"
-			+ tabs.toString() +"    \"methodtype\" : \" " + meth.getMtype() +"\",\n"
-			+ tabs.toString() +"    \"location\" : \" " + loc.getLname() +"\",\n"
-			+ tabs.toString() +"    \"country\" : \" " + cnty.getIsoabbr() +"\",\n"
-			+ tabs.toString() +"    \"ref\" : \" " + bibref.getAnalyt() +"\",\n";
-
-			for(int cntr=0;cntr<names.size();cntr++)
-			{
-				loc2 = man.getLocationByID(names.get(cntr).getLocationId());	
-				outputString = outputString 
-				+ tabs.toString() +"    \"dates"+cntr+"\" : \" " + names.get(cntr).getNdate() +"\",\n"
-				+ tabs.toString() +"    \"name"+cntr+"\" : \" " + names.get(cntr).getNval() +"\",\n"
-				+ tabs.toString() +"    \"ntype"+cntr+"\" : \" " + names.get(cntr).getTypeId() +"\",\n"
-				+ tabs.toString() +"    \"nstat"+cntr+"\" : \" " + names.get(cntr).getNstat() +"\",\n"
-				+ tabs.toString() +"    \"loc"+cntr+"\" : \" " + loc2.getLname() +"\",\n";
-
-			}
-			outputString = outputString + tabs.toString() +"    \"gpid1\" : \" " + node.getGermplasm().getGpid1() +"\",\n";
-			outputString = outputString + tabs.toString() +"    \""+"gpid2"+"\""+": \"" + node.getGermplasm().getGpid2() + "\",\n";
-		}
-
-		else{
-
-			Debug.println(0, tabs.toString() +"\"gid\" : \""+ node.getGermplasm().getGid() + "\",\n" + tabs.toString()+ "\"name\" : \""+ name +"\",\n" +tabs.toString()+ "\"layer\": \""+ (level-1) + "\"");
-
-			outputString = outputString + "\n "+ tabs.toString() + tabs.toString() + "\"gid\" : \" " + node.getGermplasm().getGid() +"\",\n"
-			+ tabs.toString() +"    \"name\" : \" " + name +"\",\n"
-			+ tabs.toString() +"    \"date\" : \" " + node.getGermplasm().getGdate() +"\",\n"
-			+ tabs.toString() +"    \"id\" : \" " + node.getGermplasm().getGid() +"\",\n"
-			+ tabs.toString() +"    \"layer\" : \" " + (level-1) +"\",\n"
-			+ tabs.toString() +"    \"methodname\" : \" " + meth.getMname() +"\",\n"
-			+ tabs.toString() +"    \"methodtype\" : \" " + meth.getMtype() +"\",\n"
-			+ tabs.toString() +"    \"location\" : \" " + loc.getLname() +"\",\n"
-			+ tabs.toString() +"    \"country\" : \" " + cnty.getIsoabbr() +"\",\n"
-			+ tabs.toString() +"    \"ref\" : \" " + bibref.getAnalyt() +"\",\n";
-
-			for(int cntr=0;cntr<names.size();cntr++)
-			{
-				loc2 = man.getLocationByID(names.get(cntr).getLocationId());	
-				outputString = outputString 
-				+ tabs.toString() +"    \"dates"+cntr+"\" : \" " + names.get(cntr).getNdate() +"\",\n"
-				+ tabs.toString() +"    \"name"+cntr+"\" : \" " + names.get(cntr).getNval() +"\",\n"
-				+ tabs.toString() +"    \"ntype"+cntr+"\" : \" " + names.get(cntr).getTypeId() +"\",\n"
-				+ tabs.toString() +"    \"nstat"+cntr+"\" : \" " + names.get(cntr).getNstat() +"\",\n"
-				+ tabs.toString() +"    \"loc"+cntr+"\" : \" " + loc2.getLname() +"\",\n";
-
-			}
-			outputString = outputString + tabs.toString() +"    \"gpid1\" : \" " + node.getGermplasm().getGpid1() +"\",\n";
-			outputString = outputString + tabs.toString() +"    \""+"gpid2"+"\""+": \"" + node.getGermplasm().getGpid2() + "\"\n";
-		}
-
-		if(!node.getLinkedNodes().isEmpty()){
-			outputString = outputString + tabs.toString() + "\"children\" : \n" + tabs.toString() +"[";
-			System.out.println(tabs.toString() + "\"children\" : \n" + tabs.toString() +"[");
-		}
-
-		for (GermplasmPedigreeTreeNode parent : node.getLinkedNodes()) {
-
-			counterArray.set(level-1,counterArray.get(level-1)+1);
-
-			System.out.println(tabs.toString()+"{");
-			outputString = outputString + "\n" + tabs.toString()+ "{" ;
-			outputString = outputString + "\n";
-			printNode(parent, level + 1);
-			outputString = outputString + "\n" + tabs.toString()+ "}";
-			System.out.println(tabs.toString()+"}");
-
-			if(counterArray.get(level-1) < node.getLinkedNodes().size() ) 
-			{
-				outputString = outputString + tabs.toString()+ ","; //check if no sibling
-				System.out.println(tabs.toString()+",");
-			}
-
-		}
-
-		if(!node.getLinkedNodes().isEmpty()){
-			outputString = outputString + "\n" + tabs.toString()+ "]";
-			System.out.println(tabs.toString()+"]");
-		}
+		
+		Germplasm germ = man.getGermplasmByGID(-129);
+		System.out.println("Here: " + germ.getGid());
 	}
+	
+	private void printNode(GermplasmPedigreeTreeNode node, int level, List <Name> names, Location loc2, List <Attribute> attributes, int cid, Country cnty, GermplasmDataManager man, Method meth, Bibref bibref, Location loc) throws IOException, MiddlewareQueryException {
+	       
+		StringBuffer tabs = new StringBuffer();
+		//ManagerFactory factory = new Config().configDB();
+		//GermplasmDataManager man = factory.getGermplasmDataManager();
+		
+		meth = man.getMethodByID(node.getGermplasm().getMethodId());
+		loc = man.getLocationByID(node.getGermplasm().getLocationId());
+		bibref = man.getBibliographicReferenceByID(node.getGermplasm().getReferenceId());
+		names = man.getNamesByGID(node.getGermplasm().getGid(), null, null);
+		loc2 = null;
+		attributes = man.getAttributesByGID(node.getGermplasm().getGid());
+		
+		cid = loc.getCntryid();
+		cnty = man.getCountryById(cid);
+		 
+        for (int ctr = 1; ctr < level; ctr++) {
+            tabs.append("\t");
+        }
+        
+        counterArray.set(level-1, 0);
+        String name = node.getGermplasm().getPreferredName() != null ? node.getGermplasm().getPreferredName().getNval() : null;
+        
+        if(!node.getLinkedNodes().isEmpty()){
+        	Debug.println(0, tabs.toString() +"\"gid\" : \""+ node.getGermplasm().getGid() + "\",\n" + tabs.toString()+ "\"name\" : \""+ name +"\",\n" +tabs.toString()+ "\"layer\": \""+ (level-1) + "\",");
+        	
+        	outputString = outputString + "\n "+ tabs.toString() + tabs.toString() +"\"gid\" : \"" + node.getGermplasm().getGid() +"\",\n"
+                    + tabs.toString() +"    \"name\" : \"" + name +"\",\n"
+                    + tabs.toString() +"    \"date\" : \"" + node.getGermplasm().getGdate() +"\",\n"
+                    + tabs.toString() +"    \"id\" : \"" + node.getGermplasm().getGid() +"\",\n"
+                    + tabs.toString() +"    \"layer\" : \"" + (level-1) +"\",\n"
+                    + tabs.toString() +"    \"methodname\" : \"" + meth.getMname() +"\",\n"
+                    + tabs.toString() +"    \"methodtype\" : \"" + meth.getMtype() +"\",\n"
+                    + tabs.toString() +"    \"location\" : \"" + loc.getLname() +"\",\n"
+                    + tabs.toString() +"    \"country\" : \"" + cnty.getIsoabbr() +"\",\n"
+                    + tabs.toString() +"    \"ref\" : \"" + bibref.getAnalyt() +"\",\n";
+            
+            for(int cntr=0;cntr<names.size();cntr++)
+     		{
+     			  loc2 = man.getLocationByID(names.get(cntr).getLocationId());	
+                  outputString = outputString 
+                  + tabs.toString() +"    \"dates"+cntr+"\" : \"" + names.get(cntr).getNdate() +"\",\n"
+                  + tabs.toString() +"    \"name"+cntr+"\" : \"" + names.get(cntr).getNval() +"\",\n"
+                  + tabs.toString() +"    \"ntype"+cntr+"\" : \"" + names.get(cntr).getTypeId() +"\",\n"
+                  + tabs.toString() +"    \"nstat"+cntr+"\" : \"" + names.get(cntr).getNstat() +"\",\n"
+                  + tabs.toString() +"    \"loc"+cntr+"\" : \"" + loc2.getLname() +"\",\n";
+                 
+    		}
+            
+            for(int cntr=0;cntr<attributes.size();cntr++)
+     		{
+     			  loc2 = man.getLocationByID(attributes.get(cntr).getLocationId());	
+                  outputString = outputString 
+                  + tabs.toString() +"    \"aid"+cntr+"\" : \"" + attributes.get(cntr).getAid() +"\",\n"
+                  + tabs.toString() +"    \"atype"+cntr+"\" : \"" + attributes.get(cntr).getTypeId() +"\",\n"
+                  + tabs.toString() +"    \"aval"+cntr+"\" : \"" + attributes.get(cntr).getAval() +"\",\n"
+                  + tabs.toString() +"    \"aloc"+cntr+"\" : \"" + loc2.getLname() +"\",\n"
+                  + tabs.toString() +"    \"adate"+cntr+"\" : \"" + attributes.get(cntr).getAdate() +"\",\n";
+                 
+    		}
+            
+            
+     		outputString = outputString + tabs.toString() +"    \"gpid1\" : \"" + node.getGermplasm().getGpid1() +"\",\n";
+     		outputString = outputString + tabs.toString() +"    \""+"gpid2"+"\""+": \"" + node.getGermplasm().getGpid2() + "\",\n";
+        }
+        	
+        else{
+        
+        	Debug.println(0, tabs.toString() +"\"gid\" : \""+ node.getGermplasm().getGid() + "\",\n" + tabs.toString()+ "\"name\" : \""+ name +"\",\n" +tabs.toString()+ "\"layer\": \""+ (level-1) + "\"");
+        
+        	outputString = outputString + "\n "+ tabs.toString() + tabs.toString() + "\"gid\" : \"" + node.getGermplasm().getGid() +"\",\n"
+                    + tabs.toString() +"    \"name\" : \"" + name +"\",\n"
+                    + tabs.toString() +"    \"date\" : \"" + node.getGermplasm().getGdate() +"\",\n"
+                    + tabs.toString() +"    \"id\" : \"" + node.getGermplasm().getGid() +"\",\n"
+                    + tabs.toString() +"    \"layer\" : \"" + (level-1) +"\",\n"
+                    + tabs.toString() +"    \"methodname\" : \"" + meth.getMname() +"\",\n"
+                    + tabs.toString() +"    \"methodtype\" : \"" + meth.getMtype() +"\",\n"
+                    + tabs.toString() +"    \"location\" : \"" + loc.getLname() +"\",\n"
+                    + tabs.toString() +"    \"country\" : \"" + cnty.getIsoabbr() +"\",\n"
+                    + tabs.toString() +"    \"ref\" : \"" + bibref.getAnalyt() +"\",\n";
+            
+            for(int cntr=0;cntr<names.size();cntr++)
+     		{
+     			  loc2 = man.getLocationByID(names.get(cntr).getLocationId());	
+                  outputString = outputString 
+                  + tabs.toString() +"    \"dates"+cntr+"\" : \"" + names.get(cntr).getNdate() +"\",\n"
+                  + tabs.toString() +"    \"name"+cntr+"\" : \"" + names.get(cntr).getNval() +"\",\n"
+                  + tabs.toString() +"    \"ntype"+cntr+"\" : \"" + names.get(cntr).getTypeId() +"\",\n"
+                  + tabs.toString() +"    \"nstat"+cntr+"\" : \"" + names.get(cntr).getNstat() +"\",\n"
+                  + tabs.toString() +"    \"loc"+cntr+"\" : \"" + loc2.getLname() +"\",\n";
+                 
+    		}
+     		outputString = outputString + tabs.toString() +"    \"gpid1\" : \"" + node.getGermplasm().getGpid1() +"\",\n";
+     		outputString = outputString + tabs.toString() +"    \""+"gpid2"+"\""+": \"" + node.getGermplasm().getGpid2() + "\"\n";
+        }
+        
+        if(!node.getLinkedNodes().isEmpty()){
+        	outputString = outputString + tabs.toString() + "\"children\" : \n" + tabs.toString() +"[";
+        	System.out.println(tabs.toString() + "\"children\" : \n" + tabs.toString() +"[");
+        }
+        
+        for (GermplasmPedigreeTreeNode parent : node.getLinkedNodes()) {
+        	
+        	counterArray.set(level-1,counterArray.get(level-1)+1);
+        	
+        	System.out.println(tabs.toString()+"{");
+        	outputString = outputString + "\n" + tabs.toString()+ "{" ;
+        	outputString = outputString + "\n";
+        	printNode(parent,level+1,names,loc2,attributes,cid,cnty,man,meth,bibref,loc);
+        	outputString = outputString + "\n" + tabs.toString()+ "}";
+            System.out.println(tabs.toString()+"}");
+           
+            if(counterArray.get(level-1) < node.getLinkedNodes().size() ) 
+            {
+             		outputString = outputString + tabs.toString()+ ","; //check if no sibling
+             		System.out.println(tabs.toString()+",");
+            }
+            
+        }
+        
+        if(!node.getLinkedNodes().isEmpty()){
+        	outputString = outputString + "\n" + tabs.toString()+ "]";
+        	System.out.println(tabs.toString()+"]");
+        }
+    }
 
 	@Path("/show_germplasm_details")
 	@POST
